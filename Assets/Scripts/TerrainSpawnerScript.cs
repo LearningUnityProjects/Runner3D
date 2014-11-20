0f672c9207f1d3b8903f0a1c09a10295a7147497
albertalma <albert.alse@gmail.com>
2014-11-20 18:34:59 +0100

    Prova spawner

---
+++ Assets/Scripts/TerrainSpawnerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainSpawnerScript : MonoBehaviour {
+
+	public GameObject[] obj;
+	public float platformSize = 15.0f;
+	public float z = -5.0f;
+	
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+	}
+	
+	void Spawn() {
+		z += platformSize;
+		Instantiate(obj[0], new Vector3(transform.position.x-10, transform.position.y, z), Quaternion.identity);
+		z += platformSize;
+		Instantiate(obj[0], new Vector3(transform.position.x-10, transform.position.y, z), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.parent.gameObject.transform.position.z >= (z-platformSize))
+			Spawn ();
+	}
+}
