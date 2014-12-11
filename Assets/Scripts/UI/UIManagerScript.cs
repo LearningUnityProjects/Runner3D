e62407cbb9dc5646debd250c4be9fa6f01b2188f
albertalma <albert.alse@gmail.com>
2014-12-11 18:42:20 +0100

    MainMenu Hecho
    Pau si quieres mirar como esta el main menu, es la escena que se llama
    Main Menu xD

---
+++ Assets/Scripts/UI/UIManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour {
+
+	public void StartGame()
+	{
+		Application.LoadLevel(1);
+	}
+
+	public void StopGame()
+	{
+		Application.Quit();
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
