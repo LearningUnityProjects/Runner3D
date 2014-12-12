3d9e719d3f60996b293631985a84ce666b59c1dc
Pau Vila Capitan <pauvilac@gmail.com>
2014-12-12 18:21:42 +0100

    paneles con paredes enteras

--- Assets/Scripts/Enemies/AirplaneScript.cs
+++ Assets/Scripts/Enemies/AirplaneScript.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class AirplaneScript : MonoBehaviour {
 
 	public GameObject explosio;
-
+	private bool right = false;
+	private Vector3 moveDirection = Vector3.zero;
+	private float speed = 6.0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,13 +14,28 @@ public class AirplaneScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		CharacterController controller = GetComponent<CharacterController>();
+		if(right)moveDirection = new Vector3(1, 0,0);
+		else moveDirection = new Vector3(-1, 0,0);
+		controller.Move(moveDirection * Time.deltaTime * speed);
 	}
 
 	void OnTriggerEnter(Collider col){
-		GameObject expl = Instantiate(explosio, transform.position, Quaternion.identity) as GameObject;
-		Destroy(gameObject); 
-		Destroy(expl, 3);
-		col.gameObject.GetComponent<DiePlayerScript>().Die();
+
+		Debug.Log ("HA PICAT AMB: " + col);
+		CharacterController controller = GetComponent<CharacterController>();
+		if (col.gameObject.name == "PACKPARET") {
+			//controller.transform.Rotate(new Vector3(0,180,0));
+			right = !right;
+		} else {
+			//			GameObject expl = Instantiate (explosio, transform.position, Quaternion.identity) as GameObject;
+			//			Destroy(gameObject); 
+			//			Destroy(expl, 3);
+			//			col.gameObject.GetComponent<DiePlayerScript>().Die();
+		}
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit col) {
+
 	}
 }
---
+++ Assets/airplaneScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spiderController : MonoBehaviour {
+	public float initSpeed = 6.0f;
+	public float jumpSpeed = 18.0F;
+	public float gravity = 20.0F;
+	private bool right = true;
+	
+	private Vector3 moveDirection = Vector3.zero;
+	private float speed;
+	private float xPosition;
+	private int lane;
+	private bool isCrouching;
+	private bool isJumping;
+	private bool isSprinting;
+	
+	void Start(){
+		CharacterController controller = GetComponent<CharacterController>();
+		xPosition = controller.transform.position.x;
+		controller.animation.CrossFade ("Run");
+		lane = -1;
+		speed = initSpeed;
+		isCrouching = false;
+		isSprinting = false;
+	}
+	
+	
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		CharacterController controller = GetComponent<CharacterController>();
+		if (hit.gameObject.name == "WALL") {
+			controller.transform.Rotate(new Vector3(0,180,0));
+			right = !right;
+		}
+	}
+	
+	
+	
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		
+		controller.animation.CrossFade ("Run");
+		if(right)moveDirection = new Vector3(1, 0,0);
+		else moveDirection = new Vector3(-1, 0,0);
+		controller.Move(moveDirection * Time.deltaTime * speed);
+		//controller.transform.rigidbody.AddForce(transform.forward * speed * Time.deltaTime, ForceMode.Acceleration);
+	}
+	
+	
+}
