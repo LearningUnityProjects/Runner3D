d318c968a1f6b8c63219a5e3c3df579ee66a5ee1
Pau Vila Capitan <pauvilac@gmail.com>
2014-12-16 18:23:25 +0100

    no message

---
+++ Assets/Scripts/FollowScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowScript : MonoBehaviour {
+
+	public GameObject player;
+	
+	private Vector3 moveDirection = Vector3.zero;
+	private float speed;
+	
+	void Start(){
+	}
+	
+	void Update() {
+		speed = player.GetComponent<MovementScript>().speed;
+		Debug.Log ("speed " + speed);
+		CharacterController controller = GetComponent<CharacterController> ();
+		moveDirection = new Vector3(0, 0, 1);
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+	
+	
+}
