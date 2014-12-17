cfe1cf2079cd281c6a3ce19257e02950682dfe69
Pau Vila Capitan <pauvilac@gmail.com>
2014-12-17 20:14:43 +0100

    script cam

---
+++ Assets/camerascript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camerascript : MonoBehaviour {
+	public Vector3 posi;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards (transform.position, posi, speed * Time.deltaTime);
+	}
+}
