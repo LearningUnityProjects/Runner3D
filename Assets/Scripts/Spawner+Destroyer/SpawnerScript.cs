aacbce3d51438b7d49086bad173b8c4eecaa249f
Albert Almà <albert.alse@gmail.com>
2014-12-17 20:13:46 +0100

    acabar l2

--- Assets/Scripts/Spawner+Destroyer/SpawnerScript.cs
+++ Assets/Scripts/Spawner+Destroyer/SpawnerScript.cs
@@ -5,15 +5,21 @@ public class SpawnerScript : MonoBehaviour {
 
 	public GameObject[] obj;
 	public GameObject[] obj2;
+	public GameObject powerup;
+	public GameObject player;
 	public GameObject door;
 	public float platformSize = 15.0f;
 	public float z = -5.0f;
 	public int numberOfPlatformsToInvoke = 4;
-	
+
+	private bool Level2;
+	private float zL2;
 	private bool firstDoor;
 
 	// Use this for initialization
 	void Start () {
+		zL2 = 600.0f;
+		Level2 = false;
 		firstDoor = true;
 		Spawn ();
 	}
@@ -21,9 +27,10 @@ public class SpawnerScript : MonoBehaviour {
 	void Spawn() {
 		for (int i = 0; i < numberOfPlatformsToInvoke; ++i) {
 			z += platformSize;
-			if (z > 600.0f) {
+			if (z > 600.0f || Level2) {
 					int range = Random.Range (0, obj2.Length);
 				if (firstDoor) {
+					zL2 = z;
 					Debug.Log ("z1: " + z);
 					float aux = z;
 					aux += platformSize/4;
@@ -44,5 +51,13 @@ public class SpawnerScript : MonoBehaviour {
 	void Update () {
 		if (transform.parent.gameObject.transform.position.z >= (z-(platformSize*4)))
 			Spawn ();
+		if (Input.GetKeyDown (KeyCode.Keypad2)) {
+			Debug.Log ("Level 2");
+						Level2 = true;
+			player.GetComponent<PlayerScript>().god = true;	
+				}
+		if (Level2 && z < zL2) {
+			player.GetComponent<PlayerScript>().god = true;		
+		} else if (Level2 && z >= zL2) player.GetComponent<PlayerScript>().god = false;	
 	}
 }
