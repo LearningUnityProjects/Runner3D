5d20481d255664a0caca2428693ebc232411b798
Albert Almà <albert.alse@gmail.com>
2014-12-17 15:35:39 +0100

    Prefabs terrain

--- Assets/Scripts/Spawner+Destroyer/DestroyerScript.cs
+++ Assets/Scripts/Spawner+Destroyer/DestroyerScript.cs
@@ -6,6 +6,9 @@ public class DestroyerScript : MonoBehaviour {
 	void OnTriggerExit(Collider other) {
 		Debug.Log(other.tag);
 		Destroy(other.gameObject);
-		if(other.gameObject.transform.parent) Destroy(other.gameObject.transform.parent.gameObject);
+		GameObject parent;
+		if (other.gameObject.tag == "Floor") {
+			Destroy (other.gameObject.transform.parent.gameObject);
+		}
 	}
 }
