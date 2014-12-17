f364b0671b606a95d76b12f12cd1c9e25725b0fe
Pau Vila Capitan <pauvilac@gmail.com>
2014-12-17 17:09:49 +0100

    Revert "sound"
    This reverts commit ffafa7af890ec920deb6992c6868e27fff3e43ab.

--- Assets/Scripts/Player/DiePlayerScript.cs
+++ Assets/Scripts/Player/DiePlayerScript.cs
@@ -6,7 +6,6 @@ public class DiePlayerScript : MonoBehaviour {
 
 	public GameObject retryDialog;
 	public GameObject highScoreDialog;
-	public AudioSource exp;
 
 	public bool NewHighScore() {
 		bool ret = false;
@@ -21,10 +20,8 @@ public class DiePlayerScript : MonoBehaviour {
 	}
 
 	public void Die() {
-		exp.Play ();
 		if (NewHighScore()) highScoreDialog.SetActive(true);
 		else retryDialog.SetActive(true);
-
 	}
 
 	// Use this for initialization
